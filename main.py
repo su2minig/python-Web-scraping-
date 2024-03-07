c29128a2a247b7b97ba844c5c0de520331440cac
su2minig <mini72667@gmail.com>
2024-03-07 15:51:37 +0900

    parameter 기본값 설정(계산기)

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def say_hello(user_name="anonymous"): # 파라미터 user_name에 기본값 anonymous를 설정
-  print("Hello", user_name)
+def plus(a=0, b=0):
+  print(a + b)
 
+def minus(a=0, b=0):
+  print(a - b)
 
-say_hello("sumin")
-say_hello()
+def multiply(a=0, b=0):
+  print(a * b)
+
+def divide(a=0, b=0):
+  print(a / b)
+
+def power(a=0, b=0):
+  print(a ** b)
+
+plus(2, 3)
+minus(3, 2)
+multiply(4, 3)
+divide(6, 2)
+power(2, 2)
+
+plus()
+minus()
+multiply()
+divide()
+power()
