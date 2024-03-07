02e4af1c0d7fa734e7324fb7feb05497d2d7c891
su2minig <mini72667@gmail.com>
2024-03-07 17:27:15 +0900

    함수의 return values

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-def plus(a=0, b=0):
-  print(a + b)
+def tax_calc(money):
+  return money * 0.35
 
-def minus(a=0, b=0):
-  print(a - b)
+def pay_tax(tax):
+  print("thank you for paying", tax)
 
-def multiply(a=0, b=0):
-  print(a * b)
-
-def divide(a=0, b=0):
-  print(a / b)
-
-def power(a=0, b=0):
-  print(a ** b)
-
-plus(2, 3)
-minus(3, 2)
-multiply(4, 3)
-divide(6, 2)
-power(2, 2)
-
-plus()
-minus()
-multiply()
-divide()
-power()
+to_pay = tax_calc(14)
+pay_tax(to_pay)
