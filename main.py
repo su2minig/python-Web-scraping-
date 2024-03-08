2fd2ee81054444c976fe79a6f39587a881181c42
su2minig <mini72667@gmail.com>
2024-03-08 20:43:06 +0900

    while

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from random import randint
 
-user_choice = int(input("Choose number"))
-
 pc_choice = randint(1, 50)
 
-if user_choice == pc_choice:
-  print("You won!")
-elif user_choice > pc_choice:
-  print("Lower!")
-elif user_choice < pc_choice:
-  print("Higher!")
+playing = True
+
+while playing:
+  user_choice = int(input("Choose number: "))
+  if user_choice == pc_choice:
+    print("You won!")
+    playing = False
+  elif user_choice > pc_choice:
+    print("Lower!")
+  elif user_choice < pc_choice:
+    print("Higher!")
