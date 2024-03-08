a10bbcf98e6b661f1784245f6cb13af819cd5bdf
su2minig <mini72667@gmail.com>
2024-03-08 18:34:56 +0900

    and & or

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-winner = 10
+age = int(input("How old are you?"))
 
-if winner > 10:
-  print("Winner is greater than 10")
-elif winner < 10:
-  print("Winner is less than 10")
+if age < 18:
+  print("You can't drink")
+elif age > 18 and age < 35: # and는 둘 다 참일 때만 참이다. 앞부분이 거짓이면 
+  print("You drink beer!")  # 뒷부분은 확인하지 않는다.
+elif age == 60 or age ==70:
+  print("Birthday party!")  # or는 둘 중 하나만 참이면 참이다.
 else:
-  print("Winner is 10")
+  print("Go ahead!")
