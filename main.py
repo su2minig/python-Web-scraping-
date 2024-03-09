7acbe6581df324cd8ad3c66d19162118db9a4b4c
su2minig <mini72667@gmail.com>
2024-03-09 16:37:15 +0900

    dict

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-days = ("mon", "tue", "wed")
+user = {
+  'name': 'sumin',
+  'age': 22,
+  'fav_food': ['pizza', 'burger'],
+}
 
-print(days[0])
-print(days.count("mon"))
+print(user.get('age'))
+print(user.get('name'))
+user.pop('age')
+print(user)
+user['level'] = 3
+print(user)
+user['fav_food'].append('ice cream')
+print(user)
