cd59f37e3f3c6aebaee0c8da9251c8fe9e61eba0
su2minig <mini72667@gmail.com>
2024-03-09 15:58:31 +0900

    튜플

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-days_of_week = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-]
+days = ("mon", "tue", "wed")
 
-print(days_of_week.count("Wednesday")) # method count()
-# 메소드는 데이터 타입에 따라 사용할 수 있는 함수를 의미한다.
-days_of_week.remove("Sunday")
-print(days_of_week)
-days_of_week.append("Sunday")
-print(days_of_week)
-print(days_of_week[3])
+print(days[0])
+print(days.count("mon"))
