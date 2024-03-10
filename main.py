50195138f52569d4cc05a8e6163e817eea8f7a72
su2minig <mini72667@gmail.com>
2024-03-10 13:36:32 +0900

    recap

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from requests import get
 
-websites = (
-    "google.com",
-    "airbnb.com",
-    "twitter.com",
-    "facebook.com",
-)
+websites=[
+"google.com",
+"https://httpstat.us/502",
+"https://httpstat.us/404",
+"https://httpstat.us/300",
+"https://httpstat.us/200",
+"https://httpstat.us/101"
+]
+#https://httpstat.us/xxx 는 http코드를 생성해주는 사이트입니다
 
-results = {}
+results={}
 
 for website in websites:
   if not website.startswith("https://"):
     website = f"https://{website}"
-  response = get(website)
-  if response.status_code == 200:
-    results[website] = "OK"
-  else:
-    results[website] = "FAILED"
+  code = get(website).status_code
+  if code >= 500:
+    results[website] = "5xx / server error"
+  elif code >= 400:
+    results[website] = "4xx / client error"
+  elif code >= 300:
+    results[website] = "3xx / redirection "
+  elif code >= 200:
+    results[website] = "2xx / successful"
+  elif code >= 100:
+    results[website] = "1xx / informational response"
 
 print(results)
