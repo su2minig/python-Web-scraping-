d7be8a2999c6059a98ea862357fd44a711ca69b7
su2minig <mini72667@gmail.com>
2024-03-10 18:18:57 +0900

    class

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 class Puppy:
 
   def __init__(self, name, breed):  # class 안에 있는 method는 자동적으로 argument 하나를 받는다.
-    print("Puppy is born")
+    print("Puppy is born")  # 데이터 설정을 위한 method
     self.name = name
     self.breedd = breed
 
-  def __str__(self):
+  def __str__(self):  # 데이터를 출력하기 위한 method
     return f"Puppy name is {self.name}, breed is {self.breedd}"
 
 ruffus = Puppy(name="ruffus", breed="golden")
