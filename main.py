5e8f89849834cee5937dbc1867ed05e9247814b2
su2minig <mini72667@gmail.com>
2024-03-10 13:25:35 +0900

    recap

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from requests import get
 
 websites = (
-  "google.com",
-  "airbnb.com",
-  "twitter.com",
-  "facebook.com",
+    "google.com",
+    "airbnb.com",
+    "twitter.com",
+    "facebook.com",
 )
+
+results = {}
 
 for website in websites:
   if not website.startswith("https://"):
     website = f"https://{website}"
-    response = get(website)
-  print(response) # 200, 429, 400, 200
+  response = get(website)
+  if response.status_code == 200:
+    results[website] = "OK"
+  else:
+    results[website] = "FAILED"
+
+print(results)
