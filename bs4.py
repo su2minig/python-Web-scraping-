3a8e6b693629027ddc175bbb86648bdbf1aadf65
su2minig <mini72667@gmail.com>
2024-03-11 18:25:09 +0900

    recap

--- bs4.py
+++ bs4.py
@@ -9,10 +9,19 @@ soup = BeautifulSoup(response.content, "html.parser")
 
 jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
 
+all_jobs = []
+
 for job in jobs:
   title = job.find("span", class_="title").text
-  region = job.find("span", class_="region").text
-  company, position, _ = job.find_all("span", class_="company")
-  conpany = company.text
-  position = position.text
-  print(f"Title: {title}", f"Region: {region}", f"Company: {company}", f"Position: {position}", sep="\n")
+  company, position, region = job.find_all("span", class_="company")
+  url = job.find("div", class_="tooltip--flag-logo").next_sibling["href"]
+  job_data = {
+      "title": title,
+      "company": company.text,
+      "position": position.text,
+      "region": region.text,
+      "url": f"https://weworkremotely.com{url}",
+  }
+  all_jobs.append(job_data)
+
+print(all_jobs)
